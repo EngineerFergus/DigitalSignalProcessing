ed1bdb2c71fc3de236e3bd794e0a76686a546051
williamskei97 <williamskei97@gmail.com>
2021-12-02 22:33:19 -0500

    Add project files.

---
+++ DigitalSignalProcessing/DSP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSignalProcessing
+{
+    public static class DSP
+    {
+        public static string TestMe()
+        {
+            return "You're linked to the DSP class";
+        }
+    }
+}
