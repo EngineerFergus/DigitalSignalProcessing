edbc8da3110786246a73b9686ac37b3ac9c5c0be
williamskei97 <williamskei97@gmail.com>
2021-12-29 23:23:14 -0500

    Began working on infastructure of showcase. Finalized charts for plotting sequences of digital data. Set up main window layout.

--- ShowCase/ViewModels/MainViewModel.cs
+++ ShowCase/ViewModels/MainViewModel.cs
@@ -6,62 +6,13 @@ using OxyPlot.Series;
 using OxyPlot.Axes;
 using DigitalSignalProcessing;
 
-namespace ShowCase
+namespace ShowCase.ViewModels
 {
     public class MainViewModel : Utils.ObservableObject
     {
-        public PlotModel MyModel { get; private set; }
-
-        private int buttonCount;
-        private double[] hammingWindow;
-        private Complex[] hammingFFT;
-
-        private string _ButtonLabel;
-        public string ButtonLabel
-        {
-            get { return _ButtonLabel; }
-            set
-            {
-                if(value != _ButtonLabel)
-                {
-                    _ButtonLabel = value;
-                    OnPropertyChanged(nameof(ButtonLabel));
-                }
-            }
-        }
-
         public MainViewModel()
         {
-            int numPoints = 1024;
-            hammingWindow = DSP.Sinc(51, 0.3);
-            hammingWindow = DSP.ZeroPad(hammingWindow, numPoints);
-            hammingFFT = DSP.FFT(hammingWindow);
-            MyModel = new PlotModel { Title = "Example 1" };
 
-            LineSeries hammingSeries = new LineSeries();
-            hammingSeries.Title = "Hamming Window";
-
-            for(int i = 0; i < hammingFFT.Length; i++)
-            {
-                //hammingSeries.Points.Add(new DataPoint((double)i / hammingFFT.Length, DSP.DB(hammingFFT[i].Magnitude)));
-                hammingSeries.Points.Add(new DataPoint((double)i / hammingFFT.Length, hammingFFT[i].Magnitude));
-                //hammingSeries.Points.Add(new DataPoint((double)i / hammingFFT.Length, hammingWindow[i]));
-            }
-
-            MyModel.Series.Add(hammingSeries);
-            MyModel.Title = "Hamming Window";
-            LinearAxis xAxis = new LinearAxis();
-            xAxis.Position = AxisPosition.Bottom;
-            xAxis.Title = "Sample Number";
-            MyModel.Axes.Add(xAxis);
-            ButtonLabel = "You've initialized!";
-            buttonCount = 0;
-        }
-
-        public void CountButtonClicked(object sender, EventArgs e)
-        {
-            buttonCount++;
-            ButtonLabel = $"You clicked the button. Count: {buttonCount}.";
         }
     }
 }
---
+++ ShowCase/ViewModels/SequencesViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalSignalProcessing;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+
+namespace ShowCase.ViewModels
+{
+    public class SequencesViewModel
+    {
+        private readonly double[] ImpulseSequence;
+        private readonly double[] StepSequence;
+        private readonly double[] RectangleSequence;
+
+        public PlotModel ImpulsePlot { get; private set; }
+        public PlotModel StepPlot { get; private set; }
+        public PlotModel RectanglePlot { get; private set; }
+
+        public SequencesViewModel()
+        {
+            int M = 32;
+            ImpulseSequence = DSP.Impulse(M);
+            StepSequence = DSP.Step(M, 1, M / 2);
+            RectangleSequence = DSP.Rectangle(M, M / 4, 1, M / 4);
+            ImpulsePlot = InitializePlotModel(ImpulseSequence, "Impulse");
+            StepPlot = InitializePlotModel(StepSequence, "Step");
+            RectanglePlot = InitializePlotModel(RectangleSequence, "Rectangle");
+        }
+
+        private static PlotModel InitializePlotModel(double[] sequence, string title)
+        {
+            PlotModel plotModel = new() { Title = title };
+            LineSeries series = new();
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                series.Points.Add(new DataPoint(i, sequence[i]));
+            }
+
+            OxyColor black = OxyColor.FromRgb(0, 0, 0);
+            OxyColor lightGray = OxyColor.FromRgb(80, 80, 80);
+            series.Color = black;
+            series.MarkerType = MarkerType.Square;
+            series.MarkerSize = 3;
+            series.MarkerFill = black;
+            series.StrokeThickness = 1;
+
+            plotModel.Series.Add(series);
+
+            LinearAxis xAxis = new()
+            {
+                Position = AxisPosition.Bottom,
+                Title = "Sample Number",
+                MajorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = lightGray
+            };
+            plotModel.Axes.Add(xAxis);
+
+            LinearAxis yAxis = new()
+            {
+                Position = AxisPosition.Left,
+                Title = "Magnitude",
+                MajorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = lightGray
+            };
+            plotModel.Axes.Add(yAxis);
+
+            return plotModel;
+        }
+    }
+}
