964237ea05050fee0fe30dcddcc8a68d6df92484
williamskei97 <williamskei97@gmail.com>
2022-01-22 20:29:16 -0500

    Added final view and view model for filter design. Implementation needed.

---
+++ ShowCase/ViewModels/FilterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DigitalSignalProcessing;
+using ShowCase.Utils;
+using OxyPlot;
+
+namespace ShowCase.ViewModels
+{
+    public class FilterViewModel
+    {
+        private IFilter filter;
+        public PlotModel FilterFrequencyPlot { get; private set; }
+        public PlotModel FilterKernelPlot { get; private set; }
+    }
+}
