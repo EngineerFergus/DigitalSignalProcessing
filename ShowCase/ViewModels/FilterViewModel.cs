5ac416bb9a4f6cb358f644fb13d597164406b7ba
williamskei97 <williamskei97@gmail.com>
2022-01-23 13:27:30 -0500

    Implemented back end for filter design. Implemented new ICommand RelayCommand for MVVM design.

--- ShowCase/ViewModels/FilterViewModel.cs
+++ ShowCase/ViewModels/FilterViewModel.cs
@@ -2,32 +2,97 @@
 using ShowCase.Utils;
 using ShowCase.Utils.Enums;
 using OxyPlot;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ShowCase.ViewModels
 {
     public class FilterViewModel : ObservableObject
     {
+        private readonly int M = 101;
         private IFilter filter;
 
-        private FilterType filterType;
-        public FilterType SelectedType
+        private double _CutoffOne;
+        public double CutoffOne
         {
-            get { return filterType; }
+            get { return _CutoffOne; }
             set
             {
-                if(value != filterType)
+                if(value != _CutoffOne)
                 {
-                    filterType = value;
+                    _CutoffOne = value;
+                    OnPropertyChanged(nameof(CutoffOne));
+                }
+            }
+        }
+
+        private double _CutoffTwo;
+        public double CutoffTwo
+        {
+            get { return _CutoffTwo; }
+            set
+            {
+                if(value != _CutoffTwo)
+                {
+                    _CutoffTwo = value;
+                    OnPropertyChanged(nameof(CutoffTwo));
+                }
+            }
+        }
+
+        private FilterType selectedType;
+        public FilterType SelectedType
+        {
+            get { return selectedType; }
+            set
+            {
+                if(value != selectedType)
+                {
+                    selectedType = value;
                     OnPropertyChanged(nameof(SelectedType));
                 }
             }
         }
+
+        private ICommand _GenerateFilterCommand;
+        public ICommand GenerateFilterCommand
+        {
+            get
+            {
+                if(_GenerateFilterCommand == null)
+                {
+                    _GenerateFilterCommand = new RelayCommand(CanGenerateFilter, OnGenerateFilter_Clicked);
+                }
+
+                return _GenerateFilterCommand;
+            }
+        }
+
         public PlotModel FilterFrequencyPlot { get; private set; }
         public PlotModel FilterKernelPlot { get; private set; }
 
         public FilterViewModel()
         {
             SelectedType = FilterType.LowPass;
+            CutoffOne = 0.2;
+            CutoffTwo = 0.3;
+
+            filter = new LowPassFilter(CutoffOne, M);
+            Complex[] freqZ = filter.FrequencyResponse();
+
+
+            FilterFrequencyPlot = ChartFormatter.FormatSpectrumMagnitudeDB(freqZ, "Spectrum");
+        }
+
+        public void OnGenerateFilter_Clicked(object data)
+        {
+
+        }
+
+        public bool CanGenerateFilter(object data)
+        {
+            return true;
         }
 
     }
