2c5da8e9c0bd6d4e6fa93954af6fce73c542c66b
williamskei97 <williamskei97@gmail.com>
2022-01-23 00:49:47 -0500

    Implemented converters for using enums as combo box options. Is very useful.

---
+++ ShowCase/Utils/Enums/FilterType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ShowCase.Utils.Enums
+{
+    public enum FilterType
+    {
+        [Description("Low Pass Filter")]
+        LowPass = 0,
+        [Description("High Pass Filter")]
+        HighPass = 1,
+        [Description("Band Pass Filter")]
+        BandPass = 2,
+        [Description("Band Reject Filter")]
+        BandReject = 3
+    }
+}
--- ShowCase/ViewModels/FilterViewModel.cs
+++ ShowCase/ViewModels/FilterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DigitalSignalProcessing;
 using ShowCase.Utils;
+using ShowCase.Utils.Enums;
 using OxyPlot;
 
 namespace ShowCase.ViewModels
@@ -7,7 +8,27 @@ namespace ShowCase.ViewModels
     public class FilterViewModel : ObservableObject
     {
         private IFilter filter;
+
+        private FilterType filterType;
+        public FilterType SelectedType
+        {
+            get { return filterType; }
+            set
+            {
+                if(value != filterType)
+                {
+                    filterType = value;
+                    OnPropertyChanged(nameof(SelectedType));
+                }
+            }
+        }
         public PlotModel FilterFrequencyPlot { get; private set; }
         public PlotModel FilterKernelPlot { get; private set; }
+
+        public FilterViewModel()
+        {
+            SelectedType = FilterType.LowPass;
+        }
+
     }
 }
